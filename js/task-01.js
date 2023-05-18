e84a81bcf8e80af549f081d9a6617c6206738744
vldslvD <dobritsavlad@gmail.com>
2023-05-18 22:48:38 +0200

    fix#2

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,10 @@
-const categoriesEl = document.querySelector("#categories");
+const categoriesEl = document.lastElementChild.lastElementChild.children[1];
 console.log("Number of categories: " + categoriesEl.children.length);
-
-const itemsEl = document.querySelectorAll("#categories .item");
-itemsEl.forEach((item) => {
-  console.log("Category: " + item.querySelector("h2").textContent);
-  console.log("Elements: " + item.querySelector("ul").children.length);
+let array = [];
+for (let i = 0; i < categoriesEl.children.length; i++){
+  array[i] = categoriesEl.children[i];
+}
+array.forEach((item) => {
+  console.log("Category: " + item.firstElementChild.textContent);
+  console.log("Elements: " + item.lastElementChild.children.length);
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,5 @@
 const inputEl = document.querySelector("#validation-input");
 
-inputEl.classList.add("invalid");
 inputEl.addEventListener("input", (event) => {
   event.currentTarget.value.length == inputEl.dataset.length
     ? inputEl.classList.replace("invalid", "valid")
